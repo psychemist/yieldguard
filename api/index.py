da27cd9d940ae674c051c1fb1dba9623ed8c6de8
psychemist <ikeagudike@gmail.com>
2025-12-12 09:40:35 +0100

    refactor: move vercel entrypoint to root api/ folder for better detectio

---
+++ api/index.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+backend_path = os.path.join(project_root, 'backend')
+if backend_path not in sys.path:
+    sys.path.append(backend_path)
+
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
+from backend.main import app
